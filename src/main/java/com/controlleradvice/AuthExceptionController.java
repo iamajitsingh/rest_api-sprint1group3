2d5306d6f37764a9cefa26fc36544ab485183924
iamajitsingh <iamajit41@gmail.com>
2022-10-22 01:31:24 +0530

    Reverted back to some changes

--- src/main/java/com/controlleradvice/AuthExceptionController.java
+++ src/main/java/com/controlleradvice/AuthExceptionController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.controllerexp.SignUpException;
 import com.controllerexp.UserNotFoundException;
 
 @ControllerAdvice
@@ -13,10 +12,5 @@ public class AuthExceptionController {
    @ExceptionHandler(value = UserNotFoundException.class)
    public ResponseEntity<Object> exception(UserNotFoundException exception) {
       return new ResponseEntity<>("Employee Not Found!", HttpStatus.NOT_FOUND);
-   }
-   
-   @ExceptionHandler(value = SignUpException.class)
-   public ResponseEntity<Object> exception(SignUpException exception) {
-      return new ResponseEntity<>("Uh Oh! An exception occured. Please try signing up again!", HttpStatus.NOT_FOUND);
    }
 }
